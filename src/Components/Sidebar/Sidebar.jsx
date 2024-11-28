fdf685a0f42ec4055c4dfd1690b54f1ac1c6fc4b
saranthecoder <115061220+saranthecoder@users.noreply.github.com>
2024-11-28 13:46:31 +0530

    first commit

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Sidebar.css";
+
+const Sidebar = ({ adminDetails }) => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar-box">
+                <h4 className="sidebar-welcome">Welcome,</h4>
+                <h1 className="sidebar-name">{adminDetails.name}</h1>
+                <p className="sidebar-id">ID: {adminDetails.ID}</p>
+            </div>
+
+            <div className="sidebar-container">
+
+
+                <button className="sidebar-container-button">
+                    <NavLink className="sidebar-container-link" to="/dashboard">
+                        Dashboard
+                    </NavLink>
+                </button>
+
+                <br />
+                <hr />
+
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
